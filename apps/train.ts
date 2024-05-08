825e16cbcffb125b7eb540c774fcc68940a24f1f
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-05-08 20:19:41 +0900

    fix zj task yechimi

---
+++ apps/train.ts
@@ -0,0 +1,26 @@
+
+
+
+ //ZJ TAsk
+ 
+ /* 
+
+ Shunday function yozing,
+   u berilgan arrayni ichidagi numberlarni qiymatini hisoblab qaytarsin.
+MASALAN: reduceNestedArray([1, [1, 2, [4]]]) return 8
+ */
+  
+function reduceNestedArray(input: Array<number | Array<any>>): number {
+    let total = 0;
+      for (const element of input) {
+          if (Array.isArray(element)) {
+              total += reduceNestedArray(element); 
+          } else {
+              total += element; 
+          }
+      }
+      return total;
+  } 
+  const result = reduceNestedArray([1, [1, 2, [4]]]);
+  console.log(result);
+ 
