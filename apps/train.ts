621268b9f683ee8a345d375b4d76884c784f6fd6
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-05-10 12:23:09 +0900

    fix: ZK-task yechimi

--- apps/train.ts
+++ apps/train.ts
@@ -1,7 +1,34 @@
 console.log("Hello World");
 
+/* 
+ZK-TASK:
 
- //ZJ TAsk
+Shunday function yozing, 
+u har soniyada bir marta consolega 1 dan 5 gacha bolgan raqamlarni chop etsin
+ va 5 soniyadan keyin ishini toxtatsin.
+MASALAN: printNumbers()
+*/
+
+function printNumber() {
+    let currentNumber = 1;
+    const intervalId = setInterval(() => {
+        console.log(currentNumber);
+        currentNumber++;
+        if (currentNumber > 5) {
+            clearInterval(intervalId); 
+        }
+    }, 1000);
+  }
+  
+  
+  setTimeout(printNumber, 5000);
+
+ 
+
+
+
+
+//ZJ TAsk
  
  /* 
 
@@ -9,7 +36,7 @@ console.log("Hello World");
    u berilgan arrayni ichidagi numberlarni qiymatini hisoblab qaytarsin.
 MASALAN: reduceNestedArray([1, [1, 2, [4]]]) return 8
  */
-  
+/*   
 function reduceNestedArray(input: Array<number | Array<any>>): number {
     let total = 0;
       for (const element of input) {
@@ -22,5 +49,5 @@ function reduceNestedArray(input: Array<number | Array<any>>): number {
       return total;
   } 
   const result = reduceNestedArray([1, [1, 2, [4]]]);
-  console.log(result);
+  console.log(result); */
  
