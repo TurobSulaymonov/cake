214ae82fd0df45cbc1a2f146c33c2d0792adbf24
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-05-10 23:59:51 +0900

    fix: modify

--- apps/nestar-api/src/database/database.module.ts
+++ apps/nestar-api/src/database/database.module.ts
@@ -4,13 +4,13 @@ import {Connection} from "mongoose";
 @Module({
     imports: [MongooseModule.forRootAsync({
         useFactory: () =>({
-            uri:process.env.NODE_ENV === "production"? process.env.MONGO_PROD : process.env.MONGO_DEV,
-        })
+            uri: process.env.NODE_ENV === "production" ? process.env.MONGO_PROD : process.env.MONGO_DEV,
+        }),
     })],
     exports: [MongooseModule],
 })
 export class DatabaseModule {
-    constructor(@InjectConnection() private readonly connection: Connection){
+    constructor(@InjectConnection() connection: Connection){
         if(connection.readyState === 1){
             console.log(`MongoDB is connected into ${process.env.NODE_ENV === "production" ? "production" : "development"} db,`)
         }
