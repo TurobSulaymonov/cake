de64b7624ec1e30060c0a1272444c16b71140306
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-05-14 17:29:12 +0900

    feat: develop global GraphQL error handling system

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -11,28 +11,18 @@ export class MemberResolver {
    
     @Mutation(() => Member)  
      public async signup(@Args('input') input: MemberInput): Promise<Member> {
-        try{
         console.log("Mutation: signup");
         console.log("input", input);
         return this.memberService.signup(input);
-    }
-    catch(err){
-     console.log("Error, Signup", err);
-     throw new InternalServerErrorException;
-    }
+
 } 
 
     @Mutation(() => Member)
      public async login(@Args('input') input: LoginInput): Promise<Member>{
-        try{
             console.log("Mutation: login");
             console.log("LogInput", input)
             return this.memberService.login(input);
-        }
-        catch(err){
-         console.log("Error, Login", err);
-         throw new InternalServerErrorException;
-        }
+     
     } 
        
     
