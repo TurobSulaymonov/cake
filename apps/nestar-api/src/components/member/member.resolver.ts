16f9e0029d6a60782f7a7c7333ac4f988a68b2ab
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-05-21 16:32:55 +0900

    feat: develop getAllMembersByAdmin APIs

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,7 +1,7 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import { UseGuards} from '@nestjs/common';
-import { AgentsInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { AgentsInquiry, LoginInput, MemberInput, MembersInquiry } from '../../libs/dto/member/member.input';
 import { Member, Members } from '../../libs/dto/member/member';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
@@ -77,22 +77,25 @@ export class MemberResolver {
     @Query(() => Members)
     public async getAgents(@Args('input') input: AgentsInquiry, @AuthMember('_id') memberId: ObjectId): Promise<Members> {
           console.log(`Query: getAgents`); 
-          return this.memberService.getAgents(memberId, input);
+          return await this.memberService.getAgents(memberId, input);
 
     }
 
     /** ADMIN **/
     @Roles(MemberType.ADMIN)
     @UseGuards(RolesGuard)
-    @Mutation(() => String)
-    public async getAllMembersByAdmin(): Promise<string> {
-        return this.memberService.getAllMembersByAdmin()
+    @Mutation(() => Members)
+    public async getAllMembersByAdmin(@Args('input') input: MembersInquiry): Promise<Members> {
+        console.log(`Mutation: getAllMembersByAdmin`);
+        return await this.memberService.getAllMembersByAdmin(input)
     }
        //Authorization ADMIN
-    @Mutation(() => String)
-    public async updateMemberByAdmin(): Promise<string>{
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation(() => Member)
+    public async updateMemberByAdmin(@Args("input") input: MemberUpdate): Promise<Member>{
         console.log("Mutation: updateMemberByAdmin");
-        return this.memberService. updateMemberByAdmin();
+        return await this.memberService. updateMemberByAdmin(input);
     }
 
 }
