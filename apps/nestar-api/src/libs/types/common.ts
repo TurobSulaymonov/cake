4ffc71654acb87c223f7e6fc21b1e4afae56351c
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-05-22 13:32:48 +0900

    feat: develop createProperty GraphQL api

--- apps/nestar-api/src/libs/types/common.ts
+++ apps/nestar-api/src/libs/types/common.ts
@@ -1,3 +1,12 @@
+import { StringValueNode } from "graphql";
+import { ObjectId } from "mongoose";
+
 export interface T{
     [key:string]: any;
     }
+
+export interface  StatisticModifier {
+    _id: ObjectId;
+    targetKey: string;
+    modifier: number;
+}
