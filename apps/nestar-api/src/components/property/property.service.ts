7f6c0c2624bc41d0ed9a8a88493e242b8216fc56
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-05-24 12:14:39 +0900

    feat: develop removePropertyByAdmin GraphQl api

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -2,7 +2,12 @@ import { BadRequestException, Injectable, InternalServerErrorException } from '@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
 import { Properties, Property } from '../../libs/dto/property/property';
-import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { 
+    AgentPropertiesInquiry, 
+    AllPropertiesInquiry, 
+    PropertiesInquiry, 
+    PropertyInput 
+} from '../../libs/dto/property/property.input';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { MemberService } from '../member/member.service';
 import { PropertyStatus } from '../../libs/enums/property.enum';
@@ -226,4 +231,44 @@ export class PropertyService {
    
     return result[0];
    }
+
+   public async updatePropertyByAdmin(input: PropertyUpdate): Promise<Property> {
+    let {propertyStatus, soldAt, deletedAt } = input;
+    const search: T = {
+        _id: input._id,
+         propertyStatus: PropertyStatus.ACTIVE,
+    };
+
+    if(propertyStatus === PropertyStatus.SOLD ) soldAt = moment().toDate();
+    else if (propertyStatus === PropertyStatus.DELETE) deletedAt = moment().toDate();
+
+    const result = await this.propertyModel
+    .findOneAndUpdate(search, input, {
+        new: true,
+    })
+    .exec();
+    
+    if(!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+    if(soldAt || deletedAt) {
+        await this.memberService.memberStatsEditor({
+    _id:result.memberId,
+    targetKey: "memberProperties",
+    modifier: -1,
+        });
+    }
+    
+    return result;
+}
+
+    public async removePropertyByAdmin(propertyId: ObjectId): Promise<Property> {
+
+     const search: T = {_id: propertyId, propertyStatus: PropertyStatus.DELETE};
+     const result = await this.propertyModel.findOneAndDelete(search).exec()
+     if(!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+
+     return result;
+
+    }
+
 }
