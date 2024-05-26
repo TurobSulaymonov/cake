361a7dd293fbd470ee61e8fbbeec914d4be7442e
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-05-26 17:03:50 +0900

    feat: develop admin related boardArticle GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -12,6 +12,8 @@ export const availablePropertySorts = [
   'propertyRank',
   'propertyPrice',
 ];
+
+export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews']
  
 
  /** IMAGE CONFIGURATION **/
